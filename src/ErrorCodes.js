997957234cff969f2730e0e54747c448d5ff140c
dgeibi <png.inside@gmail.com>
2018-04-21 22:02:51 +0800

    remove errorcodes

--- src/ErrorCodes.js
+++
@@ -1,6 +0,0 @@
-export const SEVER_UNKNOWN = 1
-export const CLIENT_UNKNOWN = 2
-export const UNAUTHORIZED = 3
-export const LOGINED = 4
-export const INVALID_INPUT = 5
-export const EBADCSRFTOKEN = 6
--- src/server/handleError.js
+++ src/server/handleError.js
@@ -1,18 +1,10 @@
-import * as ErrorCodes from '~/ErrorCodes'
-
 export default function handleError(err, req, res, next) {
   console.error(err)
   if (res.headersSent) {
     next(err)
   } else if (err.code === 'EBADCSRFTOKEN') {
-    res.status(403).json({
-      ok: false,
-      code: ErrorCodes.EBADCSRFTOKEN,
-    })
+    res.status(403).end()
   } else {
-    res.status(500).json({
-      ok: false,
-      code: ErrorCodes.SEVER_UNKNOWN,
-    })
+    res.status(500).end()
   }
 }
