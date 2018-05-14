9ce4df5d69a6cdc150ce65c6b28ea5b560d27cef
dgeibi <png.inside@gmail.com>
2018-05-14 13:00:31 +0800

    reuse common reducers

--- src/app/utils/commonModel.js
+++ src/app/utils/commonModel.js
@@ -1,48 +1,41 @@
 import merge from 'dva-model-extend'
 
+const reducers = {
+  save(state, { payload }) {
+    return { ...state, ...payload }
+  },
+  replace(state, { payload }) {
+    return payload
+  },
+  rm(state, { payload: id }) {
+    const newState = { ...state }
+    delete newState[id]
+    return newState
+  },
+  patchItem(state, { payload }) {
+    const old = state[payload.id]
+    return {
+      ...state,
+      [payload.id]: {
+        ...old,
+        ...payload,
+      },
+    }
+  },
+  patchPartial(state, { payload }) {
+    state = Object.assign({}, state)
+    Object.keys(payload).forEach(id => {
+      const item = { ...state[id], ...payload[id] }
+      state[id] = item
+    })
+    return state
+  },
+}
+
 const commonModel = namespace => (...models) =>
   merge(
     {
-      reducers: {
-        save(state, { payload }) {
-          return { ...state, ...payload }
-        },
-        replace(state, { payload }) {
-          return payload
-        },
-        rm(state, { payload: id }) {
-          const newState = { ...state }
-          delete newState[id]
-          return newState
-        },
-      },
-
-      effects: {
-        *patchItem({ payload }, { select, put }) {
-          const old = yield select(x => x[namespace][payload.id])
-          yield put({
-            type: 'save',
-            payload: {
-              [payload.id]: {
-                ...old,
-                ...payload,
-              },
-            },
-          })
-        },
-
-        *patchPartial({ payload }, { put, select }) {
-          const state = { ...(yield select(x => x[namespace])) }
-          Object.keys(payload).forEach(id => {
-            const item = { ...state[id], ...payload[id] }
-            state[id] = item
-          })
-          yield put({
-            type: 'replace',
-            payload: state,
-          })
-        },
-      },
+      reducers,
     },
     ...models,
     {
