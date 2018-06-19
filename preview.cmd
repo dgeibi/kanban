2c3baea523f9d8ff3430ff1731caf0f01670e29f
dgeibi <png.inside@gmail.com>
2018-06-19 22:56:07 +0800

    preview with mysql

--- preview.cmd
+++ preview.cmd
@@ -3,6 +3,12 @@
 set NODE_ENV=production
 call npm run -s build
 
-set SESSION_KEY=cat_sjjsj
+set HOST=127.0.0.1
+set DEBUG=app:*
+set SESSION_KEY=ACssw1231!dwee3j84fxryn
+set DB_USERNAME=root
+set DB_PASSWORD=Seven7i!
+set DB_NAME=kanban
+set DB_HOSTNAME=localhost
 
 call npm run -s start
