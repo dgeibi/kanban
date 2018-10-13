b6d0f3918d74effe8ce533c852a8027e1c9d6c7b
dgeibi <png.inside@gmail.com>
2018-10-13 16:39:06 +0800

    回滚

--- src/app/components/Toggle.js
+++ src/app/components/Toggle.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import enhanceWithClickOutside from 'react-click-outside'
 
-@enhanceWithClickOutside
 class Toggle extends React.Component {
   state = {
     clicked: false,
@@ -21,23 +19,12 @@ class Toggle extends React.Component {
     })
   }
 
-  handleClickOutside() {
-    if (this.props.onClickOutSide) {
-      this.props.onClickOutSide()
-    }
-    this.blur()
-  }
-
   render() {
-    return (
-      <div>
-        {this.props.children({
-          clicked: this.state.clicked,
-          click: this.click,
-          blur: this.blur,
-        })}
-      </div>
-    )
+    return this.props.children({
+      clicked: this.state.clicked,
+      click: this.click,
+      blur: this.blur,
+    })
   }
 }
 
