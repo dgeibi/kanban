209215657ff053de1d927b65abd45e6eb6372013
dgeibi <png.inside@gmail.com>
2020-01-21 17:52:36 +0800

    修改host

--- preview.cmd
+++ preview.cmd
@@ -3,7 +3,7 @@
 set NODE_ENV=production
 call npm run -s build
 
-set HOST=127.0.0.1
+set HOST=0.0.0.0
 set DEBUG=app:*
 set SESSION_KEYS=ACssw1231!dwee3j84fxryn,xsdfl4vrnfg39ncf22#11,mfk9jf#1cHw21111
 REM set DB_USERNAME=root
